48e647eacbc93e78d4afddd392b785d377f3c547
waterplea <alexander@inkin.ru>
2020-05-06 18:46:38 +0300

    add new packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
 import {Component, Inject} from '@angular/core';
+import {WEB_AUDIO_SUPPORT} from '@ng-web-apis/audio';
 import {GEOLOCATION_SUPPORT} from '@ng-web-apis/geolocation';
 import {INTERSECTION_OBSERVER_SUPPORT} from '@ng-web-apis/intersection-observer';
 import {MIDI_SUPPORT} from '@ng-web-apis/midi';
 import {PAYMENT_REQUEST_SUPPORT} from '@ng-web-apis/payment-request';
+import {RESIZE_OBSERVER_SUPPORT} from '@ng-web-apis/resize-observer';
 
 @Component({
     selector: 'main',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    // TODO: Issue with Ivy: https://github.com/angular/angular/issues/34267
-    readonly audioSupport = !!window.AudioContext || !!(window as any).webkitAudioContext;
-
     constructor(
         @Inject(PAYMENT_REQUEST_SUPPORT) readonly paymentRequestSupport: boolean,
         @Inject(GEOLOCATION_SUPPORT) readonly geolocationSupport: boolean,
         @Inject(INTERSECTION_OBSERVER_SUPPORT) readonly intersectionSupport: boolean,
+        @Inject(RESIZE_OBSERVER_SUPPORT) readonly resizeSupport: boolean,
         @Inject(MIDI_SUPPORT) readonly midiSupport: boolean,
+        @Inject(WEB_AUDIO_SUPPORT) readonly audioSupport: boolean,
     ) {}
 }
